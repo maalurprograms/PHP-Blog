bf27beedcd4994ce030c750f01f93c8e678d0678
Jonas Cosandey <jonascosandey@hotmail.com>
2015-12-15 21:55:04 +0100

    Login Bug Fixed

--- Login/check_user.php
+++ Login/check_user.php
@@ -1,71 +1,62 @@
-<!--<html>
-    <head>
-        <title>Projektauftrag Formular</title>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="../CSS/login_stylesheet.css">
-        <link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-        <script type="text/javascript" src="login_javascript.js"></script>
-        <!-- bootstrap -->
-        <!-- Latest compiled and minified JavaScript -->
-        <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
-        <!-- Latest compiled and minified CSS -->
-        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
-    </head>
-    <body>
-        <div id="case">
-            <div id="header">
-                Information
-            </div>
-            <div id="content">
-              <!-- -->
-              <?php
-                $SQLiteDB = new SQLite3("..\SQL\blog.db");
-                $error = "Email/Passwort ungültig<br>";
-                $linkToLogin = "<a href='login.html'>Zurück zum login</a>";
-                if (count($_POST) < 3) {
-                    $result = $SQLiteDB->querySingle("select exists(select * from users where Email = '".$_POST["email"]."')");
-                    if (!$result) {
-                        print($error);
-                        print($linkToLogin);
-                    } else {
-                        $result = $SQLiteDB->querySingle("select Password from users where Email = '".$_POST["email"]."'");
-                        if (password_verify($_POST["password"], $result)) {
-                            print("<a href='../Main/main.html'>Hier gehts zur Homepage</a>");
-                            session_start();
-                            $_SESSION["EMAIL"] = $_POST["email"];
-                            $_SESSION["USERNAME"] = $SQLiteDB->querySingle("select Username from users where Email ='".$_POST["email"]."'");
-                            $_SESSION["USERID"] = $SQLiteDB->querySingle("select UserID from users where Email ='".$_POST["email"]."'");
-                        } else {
-                            print($error);
-                            print($linkToLogin);
-                        }
-                    }
-                } else {
-                    $result = $SQLiteDB->querySingle("select exists(select * from users where Email = '".$_POST["email"]."')");
-                    if ($result) {
-                        print("Diesen User gibt es schon<br>");
-                        print("<a href='login.html'>Zurück zum login</a>");
-                    } else {
-                        if ($_POST["password"] == $_POST["password_retype"]) {
-                            $hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
-                            $SQLiteDB->exec("insert into users (Email, Username, Password) values ('".$_POST["email"]."', '".$_POST["username"]."','".$hash."')");
-                            print("Vielen Dank<br>");
-                            print("<a href='..\Main\main.html'>Hier gehts zur Homepage</a>");
-                            session_start();
-                            $_SESSION["EMAIL"] = $_POST["email"];
-                            $_SESSION["USERNAME"] = $_POST["username"];
-                            $_SESSION["USERID"] = $SQLiteDB->querySingle("select UserID from users where Email ='".$_POST["email"]."'");
-                        } else {
-                            print("Die Passwörter stimmen nicht überein<br>");
-                            print("<a href='login.html'>Zurück zum login</a>");
-                        }
-
-                    }
-                }
-              ?>
-              <!-- -->
-          <!--</div>
+<?php
+    $SQLiteDB = new SQLite3("..\SQL\blog.db");
+    $error = "Email/Passwort ungültig<br>";
+    $linkToLogin = "<a href='login.html'>Zurück zum login</a>";
+    print('
+    <div class="panel panel-default">
+        <div class="panel-heading" role="tab" id="headingOne">
+            <h4 class="panel-title">
+                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    Information
+                </a>
+            </h4>
         </div>
-    </body>
-</html>
+        <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+            <div class="panel-body">
+    ');
+    if (count($_POST) < 3) {
+        $result = $SQLiteDB->querySingle("select exists(select * from users where Email = '".$_POST["email"]."')");
+        if (!$result) {
+            print($error);
+            print($linkToLogin);
+        } else {
+            $result = $SQLiteDB->querySingle("select Password from users where Email = '".$_POST["email"]."'");
+            if (password_verify($_POST["password"], $result)) {
+                print("<a href='../Main/main.html'>Hier gehts zur Homepage</a>");
+                session_start();
+                $_SESSION["EMAIL"] = $_POST["email"];
+                $_SESSION["USERNAME"] = $SQLiteDB->querySingle("select Username from users where Email ='".$_POST["email"]."'");
+                $_SESSION["USERID"] = $SQLiteDB->querySingle("select UserID from users where Email ='".$_POST["email"]."'");
+            } else {
+                print($error);
+                print($linkToLogin);
+            }
+        }
+    } else {
+        $result = $SQLiteDB->querySingle("select exists(select * from users where Email = '".$_POST["email"]."')");
+        if ($result) {
+            print("Diesen User gibt es schon<br>");
+            print("<a href='login.html'>Zurück zum login</a>");
+        } else {
+            if ($_POST["password"] == $_POST["password_retype"]) {
+                $hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+                $SQLiteDB->exec("insert into users (Email, Username, Password) values ('".$_POST["email"]."', '".$_POST["username"]."','".$hash."')");
+                print("Vielen Dank<br>");
+                print("<a href='..\Main\main.html'>Hier gehts zur Homepage</a>");
+                session_start();
+                $_SESSION["EMAIL"] = $_POST["email"];
+                $_SESSION["USERNAME"] = $_POST["username"];
+                $_SESSION["USERID"] = $SQLiteDB->querySingle("select UserID from users where Email ='".$_POST["email"]."'");
+            } else {
+                print("Die Passwörter stimmen nicht überein<br>");
+                print("<a href='login.html'>Zurück zum login</a>");
+            }
+
+        }
+    }
+        print("
+                    </div>
+                </div>
+            </div>
+        ");
+?>
--- Login/login_javascript.js
+++ Login/login_javascript.js
@@ -9,23 +9,34 @@ function setXHTTP(section) {
     return xhttp;
 }
 
-function checkLogIn() {
-    xhhtp = setXHTTP("#content")
-    xhttp_content.open("POST", "check_user.php", true);
-    xhttp_content.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp_content.send();
+function checkLogIn(e, object) {
+    e.preventDefault();
+    xhhtp.open("POST", "check_user.php", true);
+    xhhtp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    var dataLogin = "email="+$("#login-email").val()+"&password="+$("#login-password").val();
+    var dataRegister = "email="+$("#register-email").val()+"&username="+$("#register-username").val()+"&password="+$("#register-password").val()+"&password_retype="+$("#register-password-retype").val();
+    if (object.attr("name") == "login") {
+        xhhtp.send(dataLogin);
+    } else {
+        xhhtp.send(dataRegister);
+    }
+    return false;
+}
+
+function checkPassword() {
+    if ($("#register-password").val() != $("#register-password-retype").val() || !$("#register-password").val() || !$("#register-password-retype").val() ) {
+        $('#alarmtextpw').show();
+        $('#submit').hide();
+    } else {
+        $('#submit').show();
+        $('#alarmtextpw').hide();
+    }
 }
 
 $(document).ready(function(){
+    xhhtp = setXHTTP("#case");
     $('#alarmtextpw').hide();
     $('#submit').hide();
-    $('.password').on("keyup", function(){
-        if ($("#password").val() != $("#password_retype").val() || !$("#password").val() || !$("#password_retype").val() ) {
-            $('#alarmtextpw').show();
-            $('#submit').hide();
-        } else {
-            $('#submit').show();
-            $('#alarmtextpw').hide();
-        }
-    });
+    $('.password').on("keyup", checkPassword);
+    $(".form").submit(function(e){checkLogIn(e, $(this))});
 });
